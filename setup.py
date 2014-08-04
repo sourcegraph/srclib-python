32b3b0f4995dc24535d1dc97a89c169668655ee0
Beyang Liu <beyang@sourcegraph.com>
2014-08-03 18:35:09 -0700

    setup.py, install stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='grapher',
+    version='0.0',
+    url='http://sourcegraph.com/sourcegraph/srclib-python',
+    packages=['grapher'],
+    scripts=['grapher/graph.py'],
+    author='Beyang Liu',
+    description='Generate the graph for a Python code rooted at a directory'
+    'Help: graph.py -h',
+    zip_safe=False,
+    install_requires=['jedi'],
+)
