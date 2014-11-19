c8f00eed10cb6fe8a614db78a89c4dec44d33e03
Beyang Liu <beyang@sourcegraph.com>
2014-11-19 11:34:15 -0800

    piecemeal install of requirements in graphing phase

--- python/util.go
+++ python/util.go
@@ -8,10 +8,14 @@ import (
 )
 
 func runCmdLogError(cmd *exec.Cmd) {
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stderr
-	err := cmd.Run()
+	err := runCmdStderr(cmd)
 	if err != nil {
 		log.Printf("Error running `%s`: %s", strings.Join(cmd.Args, " "), err)
 	}
+}
+
+func runCmdStderr(cmd *exec.Cmd) error {
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stderr
+	return cmd.Run()
 }
