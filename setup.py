3280c51f5c2930111f7fc48fd62778ed550bf95c
Miks Kalnins <MiksKalnins@MaikuMori.com>
2015-03-23 15:35:23 +0200

    Don't declare dependency in `setup.py`. Managed via `requirements.txt`.

--- setup.py
+++ setup.py
@@ -9,5 +9,8 @@ setup(
     description='Generate the graph for a Python code rooted at a directory'
     'Help: graph.py -h',
     zip_safe=False,
-    install_requires=['jedi'],
+    # MaikuMori(23/03/2015)
+    # Don't declare dependency here because pip will just pull jedi from pip repository instead of
+    # git+https://github.com/beyang/jedi.git@c4a51cba12bc4849e19c4aa25aeb30d66e5fd238
+    # install_requires=['jedi'],
 )
