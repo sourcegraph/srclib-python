5c3427f9ba015c0bca3401acef7fee9e62264a69
Alex Saveliev <lyolik@codeminders.com>
2015-09-23 17:15:28 +0300

    Windows support
    - building proper exe name (.exe on Windows)
    - Windows and Unix versions of virtualenv have different directories (Script vs bin), taking it into account
    - When installing virtualenv, forcing installer to run from command line instead of shell, otherwise we may get Unix-style virtualenv while Windows one is needed
    - Running python scripts via 'python' because Windows does not have a way to guess program from file content
    - Retrieving executable file location from os.Args instead of calling srclib's toolchain.Lookup
    - added some logging to grapher

--- install_env.sh
+++ install_env.sh
@@ -5,7 +5,18 @@ ENV_VERSION="13.1.0"
 
 # By usign the pprint module, which is supported with the same syntax in python 2 and 3
 # this python command can return the version as a single string for checking in bash.
-if [ "$(python -c 'from pprint import pprint; pprint(int(__import__("sys").version_info[:1][0]))')" = '2' ] ; then
+if [ "$OS" = 'Windows_NT' ] ; then
+	if [[ $(cmd /C python2.exe --version 2>&1) == $'Python 2'* ]] ; then
+		PYTHON_BIN=python2.exe
+	elif [[ "$(cmd /C python.exe --version 2>&1)" == $'Python 2'* ]] ; then
+		PYTHON_BIN=python.exe
+	else
+        echo "Error: failed to locate a version of Python 2."
+        echo "       \`srclib-python\` currently only works with Python 2."
+        echo "       You should have python.exe or python2.exe in your PATH"
+        exit 1
+	fi    
+elif [ "$(python -c 'from pprint import pprint; pprint(int(__import__("sys").version_info[:1][0]))')" = '2' ] ; then
     PYTHON_BIN=$(which python)
 elif [ "$(python2 -c 'from pprint import pprint; pprint(int(__import__("sys").version_info[:1][0]))')" = '2' ] ; then
     PYTHON_BIN=$(which python2)
@@ -18,7 +29,11 @@ fi
 # Setup virtual env.
 curl -O $ENV_URL_BASE/virtualenv-$ENV_VERSION.tar.gz
 tar xzf virtualenv-$ENV_VERSION.tar.gz
-$PYTHON_BIN virtualenv-$ENV_VERSION/virtualenv.py .env
+if [ "$OS" = 'Windows_NT' ] ; then
+	cmd /C $PYTHON_BIN virtualenv-$ENV_VERSION/virtualenv.py .env
+else
+	$PYTHON_BIN virtualenv-$ENV_VERSION/virtualenv.py .env
+fi
 
 # Cleanup.
 rm virtualenv-$ENV_VERSION.tar.gz
