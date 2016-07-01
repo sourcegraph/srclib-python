f83063c1521b183c4d02a1fadc38cfe114d1482a
Unknwon <u@gogs.io>
2016-07-02 04:45:39 +0800

    Support Python2-style "print" statements in setup.py
    * fall back to Python2 when SyntaxError on calling pydep
    * install both Python2 and Python3 in virtualenv
    * specify pip3.5 in Makefile (necessary for Travis)
    * add python2.7 syntax test case

--- Makefile
+++ Makefile
@@ -1,14 +1,16 @@
 ifeq ($(OS),Windows_NT)
 # You can't change pip.exe being in use on Windows, so we'll copy original one and use it
        PIPCMD = cmd /C .env\\Scripts\\pip-vendored.exe --isolated --disable-pip-version-check
-       MYPY = .env/Scripts/mypy
-       ENV2 = virtualenv
+       PIP2CMD = cmd /C .env\\Scripts\\pip-vendored.exe --isolated --disable-pip-version-check
        ENV = virtualenv
+       ENV2 = virtualenv
+       MYPY = .env/Scripts/mypy
 else
-       PIPCMD = .env/bin/pip
-       MYPY = .env/bin/mypy
-       ENV2 = virtualenv -p python2.7
+       PIPCMD = .env/bin/pip3.5
+       PIP2CMD = .env/bin/pip2.7
        ENV = virtualenv -p python3.5
+       ENV2 = virtualenv -p python2.7
+       MYPY = .env/bin/mypy
 endif
 
 .PHONY: install test check
@@ -28,10 +30,14 @@ $(MYPY):
 install-force: .env
 	$(PIPCMD) install . --upgrade
 	$(PIPCMD) install -r requirements.txt --upgrade
+	$(PIP2CMD) install . --upgrade
+	$(PIP2CMD) install -r requirements.txt --upgrade
 
 install: .env
 	$(PIPCMD) install .
 	$(PIPCMD) install -r requirements.txt
+	$(PIP2CMD) install .
+	$(PIP2CMD) install -r requirements.txt
 
 test: .env check
 	srclib test
--- grapher/scan.py
+++ grapher/scan.py
@@ -55,8 +55,7 @@ def find_pip_pkgs(rootdir: str) -> List:
 # Directory name for test files in common practice.
 TEST_DIR = "tests"
 
-def source_files_for_pip_unit(unit_dir: str) -> Tuple[List[str], List[str]]:
-    metadata = pydepwrap.setup_info_dir(unit_dir)
+def source_files_for_pip_unit(metadata: Dict) -> Tuple[List[str], List[str]]:
     packages, modules = [], [] # type: List[str], List[str]
     if 'packages' in metadata and metadata['packages'] is not None:
         packages.extend(metadata['packages'])
@@ -66,6 +65,7 @@ def source_files_for_pip_unit(unit_dir: str) -> Tuple[List[str], List[str]]:
         modules.extend(metadata['py_modules'])
 
     # Indicate whether this unit is in root direcotry of repository.
+    unit_dir = metadata['rootdir']
     is_root_dir = unit_dir == "."
     included_tests = False
     files = []
@@ -80,8 +80,6 @@ def source_files_for_pip_unit(unit_dir: str) -> Tuple[List[str], List[str]]:
             included_tests = pkg_path.split('/')[0] == TEST_DIR
 
         pkg_files = get_source_files(pkg_path)
-        if not is_root_dir:
-            pkg_path = os.path.join(unit_dir, pkg_path)
         for pkg_file in pkg_files:
             files.append(normalize(os.path.join(pkg_path, pkg_file)))
 
@@ -108,7 +106,7 @@ def source_files_for_pip_unit(unit_dir: str) -> Tuple[List[str], List[str]]:
 # including main unit and possible test unit.
 def pkgToUnit(pkg: Dict) -> List[Unit]:
     pkgdir = pkg['rootdir']
-    files, test_files = source_files_for_pip_unit(pkgdir)
+    files, test_files = source_files_for_pip_unit(pkg)
     pkgreqs = pydepwrap.requirements(pkgdir, True)
     deps = []
     for pkgreq in pkgreqs:
@@ -117,7 +115,7 @@ def pkgToUnit(pkg: Dict) -> List[Unit]:
             deps.append(dep)
 
     unit = Unit(
-        Name = pkg['project_name'],
+        Name = pkg['project_name'] if pkg['project_name'] is not None else pkgdir,
         Type = UNIT_PIP,
         Repo = "",       # empty Repo signals it is from this repository
         CommitID = "",
@@ -177,13 +175,20 @@ def scan(diry: str) -> None:
 
 # setup_dict_to_json_serializable_dict is copy-pasted from pydep-run.py
 def setup_dict_to_json_serializable_dict(d, **kw):
+    modules = []
+    if 'py_modules' in d:
+        modules.extend(d['py_modules'])
+    if 'modules' in d:
+        modules.extend(d['modules'])
+    if len(modules) == 0:
+        modules = None
     return {
         'rootdir': kw['rootdir'] if 'rootdir' in kw else None,
         'project_name': d['name'] if 'name' in d else None,
         'version': d['version'] if 'version' in d else None,
         'repo_url': d['url'] if 'url' in d else None,
         'packages': d['packages'] if 'packages' in d else None,
-        'modules': d['py_modules'] if 'py_modules' in d else None,
+        'modules': modules,
         'scripts': d['scripts'] if 'scripts' in d else None,
         'author': d['author'] if 'author' in d else None,
         'description': d['description'] if 'description' in d else None,
