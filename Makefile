f566b9498ab6f2e9abe967c4d55d5322e9c8a100
Unknwon <u@gogs.io>
2016-07-05 00:26:23 +0800

    Fall back to Python2 when detect SyntaxError on calling pydep

--- Makefile
+++ Makefile
@@ -2,10 +2,12 @@ ifeq ($(OS),Windows_NT)
 # You can't change pip.exe being in use on Windows, so we'll copy original one and use it
        PIPCMD = cmd /C .env\\Scripts\\pip-vendored.exe --isolated --disable-pip-version-check
        MYPY = .env/Scripts/mypy
+       ENV2 = virtualenv
        ENV = virtualenv
 else
        PIPCMD = .env/bin/pip
        MYPY = .env/bin/mypy
+       ENV2 = virtualenv -p python2.7
        ENV = virtualenv -p python3.5
 endif
 
@@ -19,6 +21,12 @@ ifeq ($(OS),Windows_NT)
 	cp .env/Scripts/pip.exe .env/Scripts/pip-vendored.exe
 endif
 
+.env2:
+	$(ENV2) .env
+ifeq ($(OS),Windows_NT)
+	cp .env/Scripts/pip.exe .env/Scripts/pip-vendored.exe
+endif
+
 $(MYPY):
 	$(PIPCMD) install mypy-lang
 
@@ -27,6 +35,10 @@ install-force: .env
 	$(PIPCMD) install -r requirements.txt --upgrade
 
 install: .env
+	$(PIPCMD) install .
+	$(PIPCMD) install -r requirements.txt
+
+install-py2: .env2
 	$(PIPCMD) install .
 	$(PIPCMD) install -r requirements.txt
 
--- grapher/pydepwrap.py
+++ grapher/pydepwrap.py
@@ -17,12 +17,29 @@ from operator import itemgetter
 def setup_dirs(rootdir: str) -> List[str]:
     return pydep.setup_py.setup_dirs(rootdir)
 
-
-def setup_info_dir(setup_dir: str) -> Any:
-    info, err = pydep.setup_py.setup_info_dir(setup_dir)
+# pydep_in_python2 uses Python2 to run pydep and returns parsed JSON object.
+# This is a KLUDGE/HACK because it introduces an implicit dependency on srclib.
+def pydep_in_python2(cmd: str, dir: str) -> Any:
+    rootdir = os.environ.get('SRCLIBPY_ROOTDIR')
+    pydep = os.path.join(rootdir, ".env", "bin", "pydep-run.py")
+    python = os.path.join(rootdir, ".env", "bin", "python2.7")
+    process = subprocess.Popen([python, pydep, cmd, dir], stdout=subprocess.PIPE)
+    out, err = process.communicate()
     if err is not None:
         raise Exception(err)
-    return info
+    data = json.loads(out.decode('utf-8'))
+    return data
+
+def setup_info_dir(setup_dir: str) -> Any:
+    try:
+        info, err = pydep.setup_py.setup_info_dir(setup_dir)
+        if err is not None:
+            raise Exception(err)
+        return info
+    except SyntaxError as e:
+        return pydep_in_python2("info", setup_dir)
+    else:
+        raise
 
 def requirements_from_requirements_txt(diry: str) -> List[Dict[str, Any]]:
     reqs, err = pydep.req.requirements_from_requirements_txt(diry)
@@ -39,7 +56,13 @@ def requirements_from_requirements_txt(diry: str) -> List[Dict[str, Any]]:
     return sorted([r.to_dict() for r in reqs], key=itemgetter('key'))
 
 def requirements(pkgdir: str, resolve: bool) -> List[Dict[str, Any]]:
-    pkgreqs, err = pydep.req.requirements(pkgdir, resolve)
-    if err is not None:
-        raise Exception(err)
-    return sorted(pkgreqs, key=itemgetter('key'))
+    try:
+        pkgreqs, err = pydep.req.requirements(pkgdir, resolve)
+        if err is not None:
+            raise Exception(err)
+        return sorted(pkgreqs, key=itemgetter('key'))
+    except SyntaxError as e:
+        pkgreqs = pydep_in_python2("dep", pkgdir)
+        return sorted(pkgreqs, key=itemgetter('key'))
+    else:
+        raise
